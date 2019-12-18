2a9c6956821b1ef36d2edfa202f910aab8a2eac4
Christopher Stöckl <christopherstoeckl1@gmail.com>
2019-12-18 20:07:39 +0100

    Implement match timer

--- src/components/Match.svelte
+++ src/components/Match.svelte
@@ -1,9 +1,13 @@
 <script>
   import ScoreDisplay from "./ScoreDisplay.svelte";
   import VolumeSlider from "./VolumeSlider.svelte";
+  import MatchTimer from "./MatchTimer.svelte";
 </script>
 
-<div class="bg-gray-100 flex flex-1">
+<div class="bg-gray-100 flex flex-1 flex-col">
+  <div class="fixed flex justify-center items-center w-full z-10 h-12">
+    <MatchTimer />
+  </div>
   <ScoreDisplay />
   <!-- <VolumeSlider /> -->
 
---
+++ src/components/MatchTimer.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { onMount } from "svelte";
+  import { Stopwatch } from "../helpers/stopwatch.js";
+
+  const interval = 1000;
+  let timer;
+  let stopwatch = new Stopwatch();
+
+  function startTimer() {
+    timer = setInterval(() => {
+      stopwatch.seconds += 1;
+
+      if (stopwatch.seconds === 60) {
+        stopwatch.minutes += 1;
+        stopwatch.seconds = 0;
+      }
+    }, interval);
+  }
+
+  onMount(() => {
+    startTimer();
+  });
+</script>
+
+<span class="px-2 text-gray-600 bg-gray-100 text-base font-medium">
+  {stopwatch}
+</span>
