1932f41f3452c237a539312953587a3b228397d0
Christopher Stöckl <christopherstoeckl1@gmail.com>
2019-12-18 20:41:37 +0100

    Implement simple new game screen

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script>
   import Match from "./components/Match.svelte";
+  import NewGame from "./components/NewGame.svelte";
   import { onMount } from "svelte";
   import { connect } from "./mqtt.js";
+  import { game_running } from "./stores.js";
 
   onMount(async () => {
     connect();
@@ -9,5 +11,9 @@
 </script>
 
 <div class="flex flex-col h-full justify-between">
-  <Match />
+  {#if $game_running === false}
+    <NewGame />
+  {:else}
+    <Match />
+  {/if}
 </div>
---
+++ src/components/NewGame.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { game_running } from "../stores.js";
+
+  function startGame() {
+    game_running.set(true);
+  }
+</script>
+
+<div class="h-full flex justify-center items-center">
+  <button
+    on:click={startGame}
+    class="bg-gray-300 hover:bg-gray-400 font-semibold shadow-lg text-lg
+    text-gray-700 py-2 px-4 rounded-full">
+    Start Game
+  </button>
+</div>
