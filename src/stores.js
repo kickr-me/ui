91c20607a9cca0f9d066497bc43f76b0bbec2846
Christopher Stöckl <christopherstoeckl1@gmail.com>
2019-12-18 20:41:37 +0100

    Implement simple new game screen

--- src/stores.js
+++ src/stores.js
@@ -1,5 +1,6 @@
 import { writable } from "svelte/store";
 
+export const game_running = writable(false);
 export const just_scored = writable(false);
 export const score_red = writable(0);
 export const score_white = writable(0);
