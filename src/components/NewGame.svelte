75ab8eeb3f76494833671c097854091f5e4baf6e
Christopher Stöckl <christopherstoeckl1@gmail.com>
2020-01-13 18:37:37 +0100

    Make start game button bigger

--- src/components/NewGame.svelte
+++ src/components/NewGame.svelte
@@ -9,8 +9,8 @@
 <div class="h-full flex justify-center items-center">
   <button
     on:click={startGame}
-    class="bg-gray-300 hover:bg-gray-400 font-semibold shadow-lg text-lg
-    text-gray-700 py-2 px-4 rounded-full">
+    class="bg-gray-300 hover:bg-gray-400 font-semibold shadow-lg text-xl
+    text-gray-700 py-4 px-6 rounded-full">
     Start Game
   </button>
 </div>
