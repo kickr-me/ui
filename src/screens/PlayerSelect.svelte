2587aba5c4e3f8b0f6c92ac8835fe401e804bb0a
Christopher Stöckl <christopherstoeckl1@gmail.com>
2021-06-30 01:21:18 +0200

    Implement player select logic

--- src/screens/PlayerSelect.svelte
+++ src/screens/PlayerSelect.svelte
@@ -2,6 +2,26 @@
   import Player from "../components/Player/Player.svelte";
   import { send } from "../mqtt";
 
+  const maxPlayerCount = 4;
+
+  let selectedPlayers: number[] = new Array(maxPlayerCount);
+  selectedPlayers.fill(undefined);
+  Object.seal(selectedPlayers);
+
+  const addOrRemovePlayer = (id: number) => {
+    const exists = selectedPlayers.includes(id);
+
+    if (exists) {
+      const index = selectedPlayers.indexOf(id);
+      selectedPlayers[index] = undefined;
+    } else {
+      const index = selectedPlayers.indexOf(undefined);
+      if (index !== -1) {
+        selectedPlayers[index] = id;
+      }
+    }
+  };
+
   function startGame() {
     const channel = "game/start";
     send(channel, "");
@@ -12,7 +32,50 @@
     const response = await fetch(`https://dashboard.kickr.me/players.json`);
     return await response.json();
   })();
+
+  $: selectedPlayerCount = selectedPlayers.filter(
+    (v) => v !== undefined
+  ).length;
 </script>
+
+<div class="relative bg-gray-100 flex flex-1 flex-col h-full">
+  <div class="flex justify-between px-4 items-center w-full z-20 h-20">
+    <div class="x-center text-gray-700 font-semibold text-xl">
+      Select {maxPlayerCount - selectedPlayerCount} more players
+    </div>
+  </div>
+  <div class="player-grid px-16 pt-4 overflow-scroll">
+    <div class="grid grid-cols-5 gap-8 pb-32">
+      {#await fetchPlayers}
+        <p>Loading players...</p>
+      {:then players}
+        {#each players as player}
+          <Player
+            {player}
+            on:click={() => addOrRemovePlayer(player.id)}
+            selected={selectedPlayers.includes(player.id)}
+            disabled={selectedPlayerCount === maxPlayerCount &&
+              !selectedPlayers.includes(player.id)}
+          />
+        {/each}
+      {:catch error}
+        <p>Can't load players!</p>
+      {/await}
+    </div>
+  </div>
+  <div
+    class="fixed flex justify-center items-start px-4 w-full z-20 h-20 bottom-0"
+  >
+    <button
+      on:click={startGame}
+      class="bg-gray-800 hover:bg-gray-700 font-semibold shadow-lg text-xl
+      text-gray-100 py-4 px-6 rounded-full z-20"
+    >
+      Start game
+    </button>
+    <div class="bottom-bar-gradient absolute w-full h-40 bottom-0 z-10" />
+  </div>
+</div>
 
 <style>
   .bottom-bar-gradient {
@@ -30,34 +93,3 @@
     height: 100%;
   }
 </style>
-
-<div class="relative bg-gray-100 flex flex-1 flex-col h-full">
-  <div class="flex justify-between px-4 items-center w-full z-20 h-20">
-    <div class="x-center text-gray-700 font-semibold text-xl">
-      Select 4 more players
-    </div>
-  </div>
-  <div class="player-grid px-16 pt-4 overflow-scroll">
-    <div class="grid grid-cols-5 gap-8 pb-32">
-      {#await fetchPlayers}
-        <p>Loading players...</p>
-      {:then players}
-        {#each players as player}
-          <Player {player}/>
-        {/each}
-      {:catch error}
-        <p>Can't load players!</p>
-      {/await}
-    </div>
-  </div>
-  <div
-    class="fixed flex justify-center items-start px-4 w-full z-20 h-20 bottom-0">
-    <button
-      on:click={startGame}
-      class="bg-gray-800 hover:bg-gray-700 font-semibold shadow-lg text-xl
-      text-gray-100 py-4 px-6 rounded-full z-20">
-      Start game
-    </button>
-    <div class="bottom-bar-gradient absolute w-full h-40 bottom-0 z-10" />
-  </div>
-</div>
--- src/stores.ts
+++ src/stores.ts
@@ -1,5 +1,10 @@
 import { writable } from "svelte/store";
 
+let Team = {
+  attack: "",
+  defense: "",
+};
+
 export const game_status = writable("stopped");
 export const just_scored = writable(false);
 export const score_red = writable(0);
@@ -7,3 +12,7 @@ export const score_white = writable(0);
 export const goals = writable([]);
 export const round = writable(1);
 export const volume = writable("50");
+export const teams = writable({
+  red: Object.create(Team),
+  white: Object.create(Team),
+});
