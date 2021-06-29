475fd3d77a683791a1d53564a6ace1e8efef78b3
Christopher Stöckl <christopherstoeckl1@gmail.com>
2021-06-30 01:21:18 +0200

    Implement player select logic

--- src/components/Player/Player.svelte
+++ src/components/Player/Player.svelte
@@ -2,15 +2,25 @@
   import PlayerAvatar from "./PlayerAvatar.svelte";
 
   export let player;
-
-  let selected = false;
-
-  function handleClick(e) {
-    selected = !selected;
-  }
+  export let selected = false;
+  export let disabled = false;
 </script>
 
-<div class="player flex flex-col items-center cursor-pointer transform active:scale-95" on:click={handleClick}>
-  <PlayerAvatar avatarUrl={player.avatar_url} {selected}/>
-  <span class="block text-center text-gray-700 font-semibold">{player.username}</span>
+<div
+  class="player flex flex-col items-center transform {!disabled
+    ? 'active:scale-95 cursor-pointer'
+    : ''}"
+  class:disabled
+  on:click
+>
+  <PlayerAvatar avatarUrl={player.avatar_url} {selected} />
+  <span class="block text-center text-gray-700 font-semibold"
+    >{player.username}</span
+  >
 </div>
+
+<style>
+  .disabled {
+    @apply filter grayscale opacity-50;
+  }
+</style>
