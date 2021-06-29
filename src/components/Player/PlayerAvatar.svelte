5f76e6912d75cf10e92261a805f4acee37624dc6
Christopher Stöckl <christopherstoeckl1@gmail.com>
2021-06-29 20:12:46 +0200

    Split player into two files

--- src/components/Player/PlayerAvatar.svelte
+++ src/components/Player/PlayerAvatar.svelte
@@ -1,12 +1,6 @@
 <script>
-  export let name;
   export let avatarUrl;
-
-  let selected = false;
-
-  function handleClick(e) {
-    selected = !selected;
-  }
+  export let selected = false;
 </script>
 
 <style>
@@ -22,12 +16,9 @@
   }
 </style>
 
-<div class="player flex flex-col items-center cursor-pointer transform active:scale-95" on:click={handleClick}>
   <div class="player__avatar mb-2 rounded-full overflow-hidden" class:selected="{selected === true}">
     <img
       src="{avatarUrl ? avatarUrl : 'https://thebark.com/sites/default/files/styles/feature/public/content/article/full/toshi-1072049-unsplash.jpg?itok=WJGzLyJ4'}"
       alt=""
       class="object-cover w-full h-full"/>
   </div>
-  <span class="block text-center text-gray-700 font-semibold">{name}</span>
-</div>
