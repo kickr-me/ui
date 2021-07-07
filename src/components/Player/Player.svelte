dd28d23d874188a366591e6f7fa8ac959d16186f
Christopher Stöckl <christopherstoeckl1@gmail.com>
2021-07-07 21:49:31 +0200

    Implement VIPs and small styling improvements

--- src/components/Player/Player.svelte
+++ src/components/Player/Player.svelte
@@ -11,18 +11,12 @@
 <div
   class="player flex flex-col items-center transform {!disabled
     ? 'active:scale-95 cursor-pointer'
-    : ''}"
+    : 'filter grayscale opacity-50'}"
   class:disabled
   on:click
 >
   <PlayerAvatar avatarUrl={player.avatar_url} {selected} {team} />
   <span class="block text-center text-gray-700 font-semibold"
-    >{player.username}</span
+    >{player.username} {player.vip ? "👑" : ""}</span
   >
 </div>
-
-<style>
-  .disabled {
-    @apply filter grayscale opacity-50;
-  }
-</style>
